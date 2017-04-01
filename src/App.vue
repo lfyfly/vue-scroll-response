3b92c7ffb8a012da00c0605bd582de42779d5b23
lfyfly <410793635@qq.com>
2017-04-01 12:18:55 +0800

    第一个版本，未分离成指令

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
-<template>
-  <div id="app">
-    <hello></hello>
-  </div>
+<template lang="pug">
+#app
+  vue-scroll-response
 </template>
 
 <script>
-import Hello from './components/Hello'
+import vueScrollResponse from './components/vue-scroll-response/vue-scroll-response.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    vueScrollResponse
   }
 }
 </script>
 
 <style>
+body,html{
+  height: 100%;
+  margin:0;
+}
+a {
+  color: #555;
+  text-decoration: none;
+}
 #app {
-
+  height: 100%;
+  overflow: hidden;
 }
 </style>
